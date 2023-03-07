59da1ca5dc3644bfc7dea2d5fcdb66c93892dc25
Pixelsuft <pixelsuft@gmail.com>
2023-03-07 20:31:01 +0700

    upd

--- goodgame/numba.py
+++ goodgame/numba.py
@@ -1,4 +1,4 @@
-def njit(nopython: bool = False, fastmath: bool = False) -> any:  # noqa
+def njit(nopython: bool = False, fastmath: bool = False, cache: bool = False) -> any:  # noqa
     def wrapper(func: any) -> any:
         return func
     return wrapper
